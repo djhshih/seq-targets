c8f68c465c5d1c0d392afc7c7c58c81da7f9fbef
Wenhao Gou <gouwh29@gmail.com>
2023-11-30 17:58:18 +0800

    Better annotation

--- src/extract_table.py
+++ src/extract_table.py
@@ -15,11 +15,34 @@ introns_raw = pdf_in.pages[4].extract_table()
 #### ~ Gene_name_1 \t Gene_name_2  \t Group \t INFO 
 #### ~ Gene_name_1: name of gene
 #### ~ Gene name_2: alternative gene name (if have)
-#### ~ Group: intron/extron
+#### ~ region: intron/extron/3utr/promoter
 #### ~ INFO: additional information
 
 
-def clean_input_string(input_string, group_name =""):
+# @ intron xx,xx-xx,xx => numbers
+def clean_intron_entry(input_entry):
+    target_intervals = input_entry.replace("intron ","").\
+        replace("introns ","").split(",")
+
+
+    full_interval = []
+    for sub_interval in target_intervals:
+        if re.search("-", sub_interval):
+            start, end = sub_interval.split("-")
+            start = int(start)
+            while(start <= int(end)):
+                full_interval.append(str(start))
+                start = start + 1
+        else:
+            full_interval.append(sub_interval)
+
+    return(",".join(full_interval))
+    
+
+
+
+
+def clean_input_string(input_string, target_region):
     alt_name = ""
     # @ Detect possible alternative name of gene:
     if re.search("\\(",input_string):
@@ -35,25 +58,36 @@ def clean_input_string(input_string, group_name =""):
     input_split = input_string.split("\n",1)
     gene_name = input_split[0].strip()
     
-    # @ Tidy rest piece
-    if len(input_split) == 2:
-        info = \
-            input_split[1].replace("\n","").\
-            replace("*","").replace("- ", "-").\
-            replace(", ",",").lower().strip()
+    # @ Extron:
+    if target_region == "extron":
+        return(f'{gene_name}\t{alt_name}\textron\t\n')
+
+
+    info = input_split[1].lower().\
+            replace("\n","").replace("*","").\
+            replace("- ", "-"). replace(", ",",").\
+            strip()
+    # @ utr, ncrna, promoter
+    if re.search("(utr)|(ncrna)|(promoter)",info):
+        return(f'{gene_name}\t{alt_name}\t{info}\t\n')
+
+    # @ introns:
     else:
-        info = ""
-    return(f'{gene_name}\t{alt_name}\t{group_name}\t{info}\n')
+        info = clean_intron_entry(info)
+        return(f'{gene_name}\t{alt_name}\tintron\t{info}\n')
+
+    
 
 
 with open("gene_list.tsv", "w+") as f:
+    f.write("gene_name_1\tgene_name_2\tregion\tinfo\n")
 
     ## ~ Tidy coding extrons
     for sub_list in extrons_raw:
         for sub_gene in sub_list:
             if sub_gene == "":
                 continue
-            f.write(clean_input_string(sub_gene, "all_extron")) 
+            f.write(clean_input_string(sub_gene, "extron")) 
 
     ## ~ Tidy selected introns
     for sub_list in introns_raw:
